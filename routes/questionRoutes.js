5746bdcc20edfe9cf6a8314c1749bd448cf462a5
pasakorn <pasakorn141@gmail.com>
2024-12-09 16:01:43 +0700

    edit validation for question

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -23,90 +23,36 @@ const questionRoute = [
   {
     method: "GET",
     path: "/api/question/{id}",
-    options: {
-      validate: {
-        params: Joi.object({
-          id: Joi.string().required(),
-        }),
-      },
-    },
     handler: questionController.getQuestionById,
   },
   {
     method: "DELETE",
     path: "/api/question/{id}",
-    options: {
-      validate: {
-        params: Joi.object({
-          id: Joi.string().required(),
-        }),
-      },
-    },
     handler: questionController.deleteQuestion,
   },
   {
     method: "POST",
     path: "/api/question/practice/check-answer",
-    options: {
-      validate: {
-        payload: Joi.object({
-          id: Joi.number().integer().required(),
-          Answer: Joi.string().trim().min(1).required(),
-        }),
-      },
-    },
     handler: questionController.checkAnswerPractice,
   },
   {
     method: "POST",
     path: "/api/question/tournament/check-answer",
-    options: {
-      validate: {
-        payload: Joi.object({
-          question_id: Joi.number().integer().required(),
-          tournament_id: Joi.number().integer().required(),
-          Answer: Joi.string().trim().min(1).required(),
-        }),
-      },
-    },
     handler: questionController.checkAnswerTournament,
   },
   {
     method: "GET",
     path: "/api/question/download/{id}",
-    options: {
-      validate: {
-        params: Joi.object({
-          id: Joi.number().integer().required(),
-        }),
-      },
-    },
     handler: questionController.downloadFile,
   },
   {
     method: "GET",
     path: "/api/questions/practice",
-    options: {
-      validate: {
-        query: Joi.object({
-          page: Joi.string().required(),
-          mode: Joi.string().required(),
-        }),
-      },
-    },
     handler: questionController.getQuestionPractice,
   },
   {
     method: "GET",
     path: "/api/questions/tournament",
-    options: {
-      validate: {
-        query: Joi.object({
-          page: Joi.number().integer().required(),
-          mode: Joi.number().integer().required(),
-        }),
-      },
-    },
     handler: questionController.getQuestionTournament,
   },
   {
